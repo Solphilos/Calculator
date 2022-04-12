ee4405c546cef7866b0e5610720b25595615b71f
solphilos <trichosirius@gmail.com>
2022-04-11 21:43:28 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+add = (a, b) => { return a + b };
+
+subtract = (a, b) => { return a - b };
+
+multiply = (a, b) => {return a * b };
+
+divide = (a, b) => {return a / b };
+
+function operate(mathFunction, a, b) {
+    mathFunction(a, b)
+ }
+
+
+
+
+
+
+
+
