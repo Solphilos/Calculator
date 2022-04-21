6eca8f29d2622cfcbb9702b0c0767cc70feb2301
solphilos <trichosirius@gmail.com>
2022-04-21 19:41:37 -0400

    add function to round floating point numbers

--- script.js
+++ script.js
@@ -21,11 +21,53 @@ divide = (a, b) => { popResult(a / b); intArray.length = 0; intArray.push( a / b
 
 
 
-//calls one of the above math functions and applies them to a and b
-operate = (operator) => {   // the operator works only on the first and second item in array
+// populates the display with given number values
+
+function popResult(result) {
+   let rounded = Math.round(result * 10000) / 10000;
+   document.querySelector('.result').innerHTML = rounded;
+      
+}                                                           
+
+// resets variables and empties array in order to clear display and start equations anew
+
+function allClear() {                             
+   intArray.length = 0;
+   popResult(0);  
+   newValue = 0;     
+   displayValue = '';
+}
+
+// creates a string of numbers, to be converted to integer and pushed into an array
+
+function pressToString(thisValue) {
+   displayValue += thisValue;
+}
+
+// converts string to an integer, to be pushed to an array
+
+function stringToInt() {
+   newValue = parseInt(displayValue);
+   
+}
+
+function pushToArray() {
+   if (intArray.length === 0) {
+      let rounded = Math.round(newValue * 10000) / 10000;
+      intArray.push(rounded);
+   }
+    displayValue = '';
+    newValue = 0;
+}
+
+
+
+// calls one of the above math functions and applies them to a and b
+
+operate = (operator) => {   
     
    if (operator === 'multiply') {
-       multiply(intArray[0], intArray[1]);     // chain equations: find way add last item pushed to array (result of first equation) as part of the equation; i.e: 4x4=8+2=10
+       multiply(intArray[0], intArray[1]);     
     }
    else if (operator === 'add') {
       add(intArray[0], intArray[1]);
@@ -39,32 +81,11 @@ operate = (operator) => {   // the operator works only on the first and second i
  }
 
 
-// creates a string of numbers, to be converted to integer and pushed into an array
-
-function pressToString(thisValue) {
-   displayValue += thisValue;
-}
-
-// converts string to an integer, to be pushed to an array
-
-function stringToInt() {
-   newValue = parseInt(displayValue);
-   console.log(newValue);
-}
-
-function pushToArray() {
-   if (intArray.length === 0) {
-      intArray.push(newValue);
-   }
-    console.log(intArray);
-    displayValue = '';
-    newValue = 0;
-}
 
 
 let button = document.querySelectorAll('div.buttonContainer button');  // returns nodelist of buttons inside buttonContainer
    for(let i = 0; i < button.length; i++) {
-   button[i].addEventListener('click', function() {                   // iterates through nodelist returning index position of buttons
+   button[i].addEventListener('click', function() {                   // iterates through nodelist returning index position of buttons ( case 0-16 )
       switch (i) {
 
       case 0 : pressToString('7');
@@ -94,8 +115,7 @@ let button = document.querySelectorAll('div.buttonContainer button');  // return
                stringToInt();
                popResult(newValue)
       break;
-      case 7 :
-               operator = 'multiply';
+      case 7 : operator = 'multiply';
                pushToArray();
       break;
       case 8 : pressToString('1');
@@ -110,54 +130,31 @@ let button = document.querySelectorAll('div.buttonContainer button');  // return
                 stringToInt();
                 popResult(newValue)
       break;
-      case 11 : 
-                operator = 'subtract';
+      case 11 : operator = 'subtract';
                 pushToArray();
-
       break;
       case 12 : pressToString('0');
                 stringToInt();
                 popResult(newValue)
       break;
-      case 13 : displayValue = '.';
-                popResult(".")
+      case 13 :  popResult('.')
       break;    
       case 14 : intArray.push(newValue)
                 operate(operator)
-                console.log(intArray) 
-                      
                 
       break;
       case 15 : operator = 'add';
                 pushToArray();
-               
-                                
-
-
       break;
       case 16 : allClear();
-      break; 
-   } 
-   
-}); 
-  }  
+      break;
+      default : popResult(0);
+} }); 
+ }  
 
 
   
- function popResult(result) {
-    document.querySelector('.result').innerHTML = result;
-       // populates display with chosen number, but replaces previous numbers. 
- }                                                           // find solution that makes typing double digits possible. i.e; 22, 45, 89, 10, etc.
-
-
-
-function allClear() {                             
-    intArray.length = 0;
-    popResult(0);  
-    newValue = 0;     
-    displayValue = '';
-}
-
+ 
       
 
 
